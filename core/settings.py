03bd1213191b785cd2cc55940be8c70e60a8fbc8
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-11-11 00:49:56 +0500

    django

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v4wpry0s!npxgaye6vcc#%szy*m)4ur9eru-n*sv!*z5f%wpbp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Local Apps
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],       # html ni django tanish uchun
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpRequest
+from uuid import uuid4
 # Create your views here.
+
+task_list = []
+
+def add_task(request: HttpRequest) -> HttpResponse:
+    if request.method == 'GET':
+        return render(request=request,template_name='add.html')
+    elif request.method == 'POST':
+      form = request.POST
+
+      title = form.get('title')
+      deadline = form.get('deadline')
+      description = form.get('description')
+      priority = form.get('priority')
+      status = form.get('status')
+
+   # validition 
+
+    if title in '':
+       return HttpResponse('title uniddingz',status=400)
+    elif deadline in '':
+       return HttpResponse('deadline unitdingz',status=400)
+    
+    elif description  in '':
+       return HttpResponse('description unitdngiz',status=400)
+    
+    elif priority not in ['low','medium','high']:
+       return HttpResponse('priority belgilang',status=400)
+    
+    elif status not in ['todo','in-progress','done']:
+       return HttpResponse('status belgilang',status=400)
+    
+    else:
+       new_task = {
+        'id':(uuid4()),
+        'title':title,
+        'deadline':deadline,
+        'description':description,
+        'priority':priority,
+        'status':status
+        }
+       task_list.append(new_task)
+
+       return HttpResponse(task_list)
+    
+       
+    
